4d93cf0e68c9c662e0029c4db08429f1b2fc47ed
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2024-03-26 23:43:37 +0100

    fetaure/Added pagination on Bird List
    NB - https://github.com/michaelbromley/ngx-pagination?tab=readme-ov-file

--- frontend/src/app/components/bird-list/bird-list.component.ts
+++ frontend/src/app/components/bird-list/bird-list.component.ts
@@ -3,17 +3,19 @@ import { Bird } from '../../classes/bird.model';
 import { BirdService } from '../../services/bird.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination'; // https://github.com/michaelbromley/ngx-pagination?tab=readme-ov-file
 
 @Component({
   selector: 'app-bird-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './bird-list.component.html',
   styleUrl: './bird-list.component.css',
   providers: [BirdService]
 })
 export class BirdListComponent implements OnInit{
   birds!: Bird[];
+  p: number = 1;
 
   constructor(private birdService: BirdService, private router: Router) {
 
