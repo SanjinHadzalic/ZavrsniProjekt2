138ef92c55474247012a5da27e4ae5f0647ee56f
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2024-03-29 00:24:49 +0100

    feature/Implemented slf4j in Controllers with terminal output

--- backend/src/main/java/org/springframework/boot/backend/controller/BirdController.java
+++ backend/src/main/java/org/springframework/boot/backend/controller/BirdController.java
@@ -1,6 +1,7 @@
 package org.springframework.boot.backend.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.backend.entity.Bird;
 import org.springframework.boot.backend.service.BirdService;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +13,19 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/birds")
 @AllArgsConstructor
+@Slf4j
 public class BirdController {
     private BirdService birdService;
 
     @GetMapping
     List<Bird> all() {
+        log.info("Called method getAllBirds()");
         return birdService.getAllBirds();
     }
 
     @GetMapping("/{id}")
     Optional<Bird> getBirdById(@PathVariable Long id) {
+        log.info("Called method getBirdById() with id: " + id);
         return birdService.getBird(id);
     }
 }
--- backend/src/main/java/org/springframework/boot/backend/controller/HomeController.java
+++ backend/src/main/java/org/springframework/boot/backend/controller/HomeController.java
@@ -6,6 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
-
-
 }
