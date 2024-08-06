5c5c589dc0e1c8bc5694dc200fcf89d8297ab042
Sanjin Hadzalic <sh14.sanjin@yahoo.com>
2024-08-06 23:59:51 +0200

    feature/Added jwtInterceptor

---
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { Token } from '@angular/compiler';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const jwtToken = localStorage.getItem('token')
+
+  if(jwtToken) {
+    try{
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${jwtToken}`)
+      });
+      return next(authReq);
+    } catch(error) {
+      console.error('Error in JWT Interceptor!', error);
+    }
+  } 
+  
+  return next(req);
+};
